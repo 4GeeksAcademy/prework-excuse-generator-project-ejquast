bdde44bd7255c20ed8bc3670620205ffc20abca9
EQ <167933418+ejquast@users.noreply.github.com>
2024-06-09 03:37:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function chooseRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const builtExcuse = `${chooseRandomItem(who)} ${chooseRandomItem(
+    action
+  )} ${chooseRandomItem(what)} ${chooseRandomItem(when)}.`;
+  const excuseH1 = document.querySelector("#excuse");
+  excuseH1.textContent = builtExcuse;
 };
